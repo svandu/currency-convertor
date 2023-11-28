ff95cc859e954d10987bf79917cc7b480bb672c4
Surbhi Vandana <svandu08@gmail.com>
2023-11-28 20:54:29 +0530

    input box component made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
+import {InputBox} from "./componenets"
+import useCurrencyInfo from "./hooks/useCurrencyInfo"
+
 function App() {
 
+  const [amount, setAmount] = useState(0);
+  const [from , setFrom] = useState("usd");
+  const [to, setTo] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo()
+ 
   return (
     <>
       <h1 className='text-3xl bg-orange-500'>Currency App</h1>
